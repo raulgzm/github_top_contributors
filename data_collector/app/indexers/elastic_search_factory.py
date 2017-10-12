ca642582dd93a13b7ffff2c31ef37faf56dbb63d
raulgonz <raulgonz@mercadona.es>
2017-10-12 18:31:34 +0200

    Init index in elastic search

--- data_collector/app/indexers/elastic_search_factory.py
+++ data_collector/app/indexers/elastic_search_factory.py
@@ -5,12 +5,32 @@
 # Third-Party imports
 from elasticsearch import Elasticsearch
 # Apps Imports
-from app.settings.config import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT
+from app.settings.config import (
+	ELASTICSEARCH_HOST, 
+	ELASTICSEARCH_PORT,
+	ELASTICSEARC_INDEX_NAME,
+)
 
+
+user_mapping = {
+    "settings": {
+        "number_of_shards": 3,
+        "number_of_replicas": 2
+    },
+    "user": {
+        "properties": {
+            "id": {"type": "string"},
+            "name": {"type": "string"},
+            "location": {"type": "string"},
+            "contributions": {"type": "int"},
+        }
+    }
+}
 
 class ElasticSearchFactory(object):
 
 	__instance = None
+	index_name = ELASTICSEARC_INDEX_NAME
 
 	def __new__(self):
 		if not self.__instance:
@@ -18,4 +38,13 @@ class ElasticSearchFactory(object):
 				'host': ELASTICSEARCH_HOST, 
 				'port': ELASTICSEARCH_PORT
 			}])
+		self.init_idex()
 		return self.__instance
+
+	@classmethod
+	def init_idex(cls):
+		if not cls.__instance.indices.exists(cls.index_name):
+			cls.__instance.indices.create(
+                index=cls.index_name,
+                body=user_mapping
+            )

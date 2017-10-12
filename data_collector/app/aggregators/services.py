6a625306f21fafb2ec8a7b09329a13f4bf204968
raulgonz <raulgonz@mercadona.es>
2017-10-12 17:55:03 +0200

    Testing aggregator services

--- data_collector/app/aggregators/services.py
+++ data_collector/app/aggregators/services.py
@@ -7,14 +7,18 @@ from github import RateLimitExceededException
 # Apps Imports
 from app.github.connector import GithubConnSingleton
 
-def get_github_users_by_location(location):
-	try:
-		github_handler = GithubConnSingleton()
-		user_paginated_list = github_handler.search_users(
-			query='', 
-			**{'location': 'barcelona'}
-		)
-		for user in user_paginated_list:
-			yield user
-	except RateLimitExceededException:
-		raise StopIteration()
+
+class AggregatorServices(object):
+
+	@classmethod
+	def get_github_users_by_location(cls, location):
+		try:
+			github_handler = GithubConnSingleton()
+			user_paginated_list = github_handler.search_users(
+				query='', 
+				**{'location': location}
+			)
+			for user in user_paginated_list:
+				yield user
+		except RateLimitExceededException:
+			raise StopIteration()
---
+++ data_collector/app/aggregators/tests/test_services.py
@@ -0,0 +1,28 @@
+# Python imports
+import unittest
+# Core Flask imports
+
+# Third-Party imports
+import mock
+from github.NamedUser import NamedUser
+# Apps Imports
+from app.aggregators.services import AggregatorServices
+
+
+class AggregatorsServicesTestSuite(unittest.TestCase):
+
+	def setUp(self):
+		self.aggregator = AggregatorServices()
+
+	def test_get_github_users_by_location(self):
+		user_generator = self.aggregator.get_github_users_by_location(location='barcelona')
+		self.assertIsInstance(next(user_generator), NamedUser)
+
+	def test_get_github_users_by_location_stop_iteration(self):
+		with mock.patch('app.aggregators.services.AggregatorServices.get_github_users_by_location') as mock_get:
+			mock_get.side_effect=StopIteration()
+			with self.assertRaises(StopIteration):
+				self.aggregator.get_github_users_by_location(location='barcelona')
+
+if __name__ == '__main__':
+    unittest.main()

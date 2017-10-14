734e53c0a8d9b5a3dd8233e85a8bdfe611179445
raulgonz <raulgonz@mercadona.es>
2017-10-14 10:20:14 +0200

    Using config for locations

--- data_collector/app/aggregators/tasks.py
+++ data_collector/app/aggregators/tasks.py
@@ -6,10 +6,11 @@
 from celery import shared_task
 # Apps Imports
 from app.aggregators.services import AggregatorServices
+from app.settings.config import LOCATIONS_ENABLED
 
 @shared_task(name="run_github_users_aggregator")
 def run_github_users_aggregator():
-    AggregatorServices.run_github_aggregator(location='barcelona')
+    AggregatorServices.run_github_aggregator(location=LOCATIONS_ENABLED[0])
 
 @shared_task(name="index_user_contributions")
 def index_user_contributions(user_id, user_profile_page, user_serializer_data):
